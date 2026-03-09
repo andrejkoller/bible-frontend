0dcc8c42eb2869b95f773b666ed9d1262ab77abd
Andrej Koller <andrejkoller@outlook.com>
2026-03-09 18:53:49 +0100

    add more font variants and redesign home page

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -5,24 +5,18 @@ const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="home-container">
-      <div className="home-content">
-        <div className="brand">
-          <h1>BibleVersion</h1>
-          <p>
-            The Bible is a collection of religious texts or scriptures sacred to
-            Christians. Dive into the Bible and start reading today.
-          </p>
-        </div>
-        <div className="button-container">
-          <Button
-            variant="contained"
-            onClick={() => navigate("/")}
-            color="primary"
-          >
-            Start Reading
-          </Button>
-        </div>
+    <div className="home">
+      <div className="introduction">
+        <h1>BibleVersion</h1>
+        <p>
+          The Bible is a collection of religious texts or scriptures sacred to
+          Christians. Dive into the Bible and start reading today.
+        </p>
+      </div>
+      <div className="button-container">
+        <Button variant="contained" onClick={() => navigate("/")}>
+          Start your journey
+        </Button>
       </div>
     </div>
   );
